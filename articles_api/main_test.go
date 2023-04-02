2a9829e8c3158dbd105d098137d93585abcb2615
Byron Katz <b.katz@obscuritylabs.com>
2023-04-02 14:00:22 -0400

    chore: improve CI/CD for articles API

--- articles_api/main_test.go
+++ articles_api/main_test.go
@@ -17,31 +17,31 @@ func Test_main(t *testing.T) {
 		route        string // route path to test
 		expectedCode int    // expected HTTP status code
 	}{
-		 // First test case
-		 {
+		// First test case
+		{
 			description:  "should get HTTP status 200 when asking for an article",
 			route:        "/article",
 			expectedCode: 200,
-		  },
-		  // Second test case
-		  {
+		},
+		// Second test case
+		{
 			description:  "should get HTTP status 404, when route does not exist",
 			route:        "/not-found",
 			expectedCode: 404,
-		  },
+		},
 	}
-	
-	for _, test := range tests {
-	 // Create a new http request with the route from the test case
-	 req := httptest.NewRequest("POST", test.route, nil)
 
-	 // Perform the request plain with the app,
-	 // the second argument is a request latency
-	 // (set to -1 for no latency)
-	 resp, _ := app.Test(req, 1)
- 
-	 // Verify, if the status code is as expected
-	 assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
-   }
+	for _, test := range tests {
+		// Create a new http request with the route from the test case
+		req := httptest.NewRequest("POST", test.route, nil)
+
+		// Perform the request plain with the app,
+		// the second argument is a request latency
+		// (set to -1 for no latency)
+		resp, _ := app.Test(req, 1)
+
+		// Verify, if the status code is as expected
+		assert.Equalf(t, test.expectedCode, resp.StatusCode, test.description)
+	}
 
 }
